2f54deaa2b301faba81d73e89849013d5ca77874
Kinsly <k.kinsly@gmail.com>
2024-07-06 21:25:21 +0530

    setting orders controller and model

---
+++ app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Order extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'customer_name',
+        'process_id',
+        'value',
+        'status',
+        'user_id'
+    ];
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $username = "";
+    if(Auth::check()) $username = Auth::user()->name;
     return [
         'Laravel' => app()->version(),
         'csrf' => csrf_token(),
-        'user' => Auth::user()->name,
-
+        'user' => $username
 ];
 });
+
 
 require __DIR__.'/auth.php';
